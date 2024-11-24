e0ca0ed04f7c9ac291ba445c1d608cb82035aa36
natalia peña <penanatalia56@gmail.com>
2024-11-24 01:24:11 -0500

    creacion de formularios,  y listas con editar y eliminar

---
+++ product-order-management/src/services/datos.js
@@ -0,0 +1,9 @@
+export const mockProductos = [
+    { id: 1, nombre: 'Producto A', precio: 100 },
+    { id: 2, nombre: 'Producto B', precio: 200 },
+  ];
+  
+  export const mockOrdenes = [
+    { id: 1, productId: 1, cantidad: 2, total: 200 },
+    { id: 2, productId: 2, cantidad: 7, total: 1400 },
+  ];
